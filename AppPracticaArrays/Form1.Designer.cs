0d0f12e10ddcd0ec1fa56c6be42c5446be2e9e95
Erick Garces <Erick Garces@ERICK>
2019-11-27 13:00:23 -0500

    Proyecto Vectores

--- AppPracticaArrays/Form1.Designer.cs
+++ AppPracticaArrays/Form1.Designer.cs
@@ -28,13 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSuma = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPromedio = new System.Windows.Forms.TextBox();
+            this.lstNum = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(145, 28);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ingrese un número:";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(327, 44);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(297, 86);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(138, 23);
+            this.btnMostrar.TabIndex = 4;
+            this.btnMostrar.Text = "Mostrar Resultado";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(280, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Promedio:";
+            // 
+            // txtSuma
+            // 
+            this.txtSuma.Location = new System.Drawing.Point(365, 206);
+            this.txtSuma.Name = "txtSuma";
+            this.txtSuma.Size = new System.Drawing.Size(50, 20);
+            this.txtSuma.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(297, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Suma:";
+            // 
+            // txtPromedio
+            // 
+            this.txtPromedio.Location = new System.Drawing.Point(365, 175);
+            this.txtPromedio.Name = "txtPromedio";
+            this.txtPromedio.Size = new System.Drawing.Size(50, 20);
+            this.txtPromedio.TabIndex = 8;
+            // 
+            // lstNum
+            // 
+            this.lstNum.FormattingEnabled = true;
+            this.lstNum.Location = new System.Drawing.Point(70, 121);
+            this.lstNum.Name = "lstNum";
+            this.lstNum.Size = new System.Drawing.Size(159, 134);
+            this.lstNum.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Lista de enteros ingresados:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(447, 270);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lstNum);
+            this.Controls.Add(this.txtPromedio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSuma);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumero);
+            this.Name = "Form1";
+            this.Text = "Arrays en C#";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSuma;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPromedio;
+        private System.Windows.Forms.ListBox lstNum;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- AppPracticaArrays/Form1.cs
+++ AppPracticaArrays/Form1.cs
@@ -12,9 +12,67 @@ namespace AppPracticaArrays
 {
     public partial class Form1 : Form
     {
+        private int cont = 0;
+        private const int MAX= 100;
+        private double[] vector = new double[MAX];
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try {
+                if (cont < MAX)
+                {
+                    vector[cont] = int.Parse(this.txtNumero.Text);
+                    llenarlista();
+                    MessageBox.Show("Elemento agregado");
+                }
+                cont++;
+                
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());            
+            }
+        }
+        private void llenarlista()
+        {
+            // agregar elemento al listbox
+            this.lstNum.Items.Clear();
+            for (int i = 0; i <= cont; i++)
+            {
+                this.lstNum.Items.Add(vector[i]);
+            }
+        }
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            this.txtSuma.Text = sumatoria().ToString();
+            this.txtPromedio.Text = promedio().ToString();
+        }
+
+        private double sumatoria()
+        {
+            double suma = 0;
+            for (int i = 0; i < cont; i++)
+            {
+                suma = suma + vector[i];
+            }
+            return suma;
+        }
+        private double promedio()
+        {
+            return (sumatoria() / cont);
+        }
+
     }
 }
